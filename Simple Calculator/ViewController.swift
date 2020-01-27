46b40a1703b3e77dbb51f6ad44a8b5105ac544cc
rasyidrw <rasyidrespati.30@gmail.com>
2020-01-27 14:24:49 +0700

    Initial Commit

---
+++ Simple Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Calculator
+//
+//  Created by Rasyid Respati Wiriaatmaja on 27/01/20.
+//  Copyright © 2020 rasyidrw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
